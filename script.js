75ab7e4b816619dff8103c2ed8a47a13005ab427
puyuyu1234 <145762224+puyuyu1234@users.noreply.github.com>
2023-09-23 20:33:36 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 "use strict"
 
 let score = 0
+let spc = 0
 
 document.getElementById("tulip").addEventListener("click", function() {
-    score++
-    document.getElementById("score-value").textContent = score
+    score += spc
+    calcScore()
 })
 
 
@@ -13,6 +14,7 @@ saveButton.addEventListener("click", function() {
     
     document.cookie = "score=" + score
     document.cookie = "numItems=" + JSON.stringify(numItems)
+    document.cookie = "numUpgrades=" + JSON.stringify(numUpgrades)
     if(document.cookie){
         saveButton.textContent = "saved!"
     }
@@ -38,42 +40,84 @@ function getCookie() {
             numItems[i] = itemDatas[i]
         }
         
+        let upgradeDatas = JSON.parse(res["numUpgrades"] || "[]")
+        for(let i in upgradeDatas){
+            numUpgrades[i] = upgradeDatas[i]
+        }
+        
     }
 }
 
 const shopTable = document.getElementById("shop")
-let upgrades = [
+const upgradeTable = document.getElementById("upgrade")
+let shopItems = [
     ["🥺", 10, 1.1, 1],
     ["🤶", 100, 1.1, 10],
+    ["🤥", 500, 1.1, 15],
+    ["🏕", 10000, 1.1, 300],
 ]
-let numItems = new Array(upgrades.length).fill(0)
+let upgrades = [
+    ["☝", 10, 5, 3],
+    ["🥺", 100, 10, 2],
+    ["🤶", 1000, 10, 2],
+    ["🤥", 1000, 10, 2],
+    ["🏕", 100000, 10, 2],
+]
+let numItems = new Array(shopItems.length).fill(0)
+let numUpgrades = new Array(upgrades.length).fill(0)
 let sps = 0
 
 function init() {
     getCookie()
     makeLayout()
-    document.getElementById("score-value").textContent = score;
+    calcScore()
     calcSps()
 }
 
 function makeLayout() {
-    for(let id in upgrades){
+    for(let id in shopItems){
         let tr = document.createElement("tr")
         shopTable.appendChild(tr)
-        let item = upgrades[id]
+        let item = shopItems[id]
         let td = document.createElement("td")
         let cost = (item[1] * item[2] ** numItems[id]) | 0
         td.id = "item" + id
         td.className = "item noselect"
-        td.textContent = item[0] + "x" + numItems[id] + ", cost:" + cost + ", 🌷+" + item[3]
+        td.textContent = item[0] + "x" + numItems[id] + ", cost:" + cost + ", 🌷+" + item[3] * numItems[id] * upgrades[+id+1][3] ** numUpgrades[+id+1]
         td.addEventListener("click", function() {
             if (score >= cost){
                 score -= cost
                 numItems[id]++
                 cost = (item[1] * item[2] ** numItems[id]) | 0
                 calcSps()
-                td.textContent = item[0] + "x" + numItems[id] + ", cost:" + cost + ", 🌷+" + item[3]
-                document.getElementById("score-value").textContent = score;
+                td.textContent = item[0] + "x" + numItems[id] + ", cost:" + cost + ", 🌷+" + item[3] * numItems[id] * upgrades[+id+1][3] ** numUpgrades[+id+1]
+                calcScore()
+            }
+        })
+        tr.appendChild(td)
+    }
+    
+    
+    for(let id in upgrades){
+        let tr = document.createElement("tr")
+        upgradeTable.appendChild(tr)
+        let item = upgrades[id]
+        let td = document.createElement("td")
+        let cost = (item[1] * item[2] ** numUpgrades[id]) | 0
+        td.id = "upgrade" + id
+        td.className = "item noselect"
+        td.textContent = item[0] + "x" + numUpgrades[id] + ", cost:" + cost + ", 🌷x" + item[3]
+        td.addEventListener("click", function() {
+            if (score >= cost){
+                score -= cost
+                numUpgrades[id]++
+                cost = (item[1] * item[2] ** numUpgrades[id]) | 0
+                calcSps()
+                td.textContent = item[0] + "x" + numUpgrades[id] + ", cost:" + cost + ", 🌷x" + item[3]
+                
+                let td2 = document.getElementById("item" +(+id-1))
+                td2.textContent = shopItems[id-1][0] + "x" + numItems[id-1] + ", cost:" + ((shopItems[id-1][1] * shopItems[id-1][2] ** numItems[id-1]) | 0) + ", 🌷+" + shopItems[id-1][3] * numItems[id-1] * upgrades[id][3] ** numUpgrades[id]
+                calcScore()
             }
         })
         tr.appendChild(td)
@@ -83,19 +127,21 @@ function makeLayout() {
 
 function calcSps() {
     sps = 0
-    for(let id in upgrades){
-        let item = upgrades[id]
-        sps += item[3] * numItems[id]
+    for(let id in shopItems){
+        let item = shopItems[id]
+        sps += item[3] * numItems[id] * upgrades[+id+1][3] ** numUpgrades[+id+1]
     }
+    spc = upgrades[0][3] ** numUpgrades[0]
     
     document.getElementById("sps-value").textContent = sps;
+    document.getElementById("spc-value").textContent = spc;
     
     return
 }
 
 function makeShop() {
-    for(let id in upgrades){
-        let item = upgrades[id]
+    for(let id in shopItems){
+        let item = shopItems[id]
         let cost = (item[1] * item[2] ** numItems[id]) | 0
         
         let td = document.getElementById("item" + id)
@@ -106,14 +152,32 @@ function makeShop() {
         }
         
     }
+    for(let id in upgrades){
+        let item = upgrades[id]
+        let cost = (item[1] * item[2] ** numUpgrades[id]) | 0
+        
+        let td = document.getElementById("upgrade" + id)
+        if(score >= cost){
+            td.className = "item noselect"
+        }else{
+            td.className = "item noselect gray"
+        }
+        
+    }
 }
 
 function loop() {
-    score += sps / 10 | 0
+    score += sps / 10
     
     makeShop()
-    document.getElementById("score-value").textContent = score;
+    calcScore()
 }
+
+function calcScore() {
+    document.getElementById("score-value").textContent = score|0;
+}
+
+
 
 
 
